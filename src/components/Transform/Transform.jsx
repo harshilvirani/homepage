88444218fd018ff86556c02cdeb674ccf846f4a9
harshil-virani <harshilvirani072@gmail.com>
2023-08-07 14:59:35 +0530

    initial commit

--- src/components/Transform/Transform.jsx
+++ src/components/Transform/Transform.jsx
@@ -6,22 +6,21 @@ const Transform = () => {
     return (
         <div className={classes.box}>
             <div className={classes.Transform}>
-                <div className={classes.overlap}>
-                    {/*<div className={classes.img}>*/}
-                        {/*<div className={classes.rectangle}></div>*/}
-                        {/*<img className={classes.im} src={img} alt=" "/>*/}
+                {/*<div className={classes.overlap}>*/}
+                {/*    <div className={classes.rectangle}>*/}
+                        <img className={classes.im} src={img} alt=" "/>
                     {/*</div>*/}
-                    <div className={classes.textBox}>
-                        <p className={classes.text}>Transform Your Company by Boosting Skills and Building Teams</p>
-                        <p className={classes.text2}>General Assembly's commitment to cultivating in-demand skills and
-                            leading-edge talent extends to your workforce.
-                            <br/>
-                            <br/>
-                            We Transform companies of all sizes by providing, assessing, and training talent in today's
-                            top tech, design, and business strategies.</p>
-                        <button className={classes.btn}>Learn More</button>
-                    </div>
+                <div className={classes.textBox}>
+                    <p className={classes.text}>Transform Your Company by Boosting Skills and Building Teams</p>
+                    <p className={classes.text2}>General Assembly's commitment to cultivating in-demand skills and
+                        leading-edge talent extends to your workforce.
+                        <br/>
+                        <br/>
+                        We Transform companies of all sizes by providing, assessing, and training talent in today's
+                        top tech, design, and business strategies.</p>
+                    <button className={classes.btn}>Learn More</button>
                 </div>
+                {/*</div>*/}
             </div>
         </div>
     )
