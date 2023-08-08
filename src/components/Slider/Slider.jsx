819021fbe331b200180f1c4bfa3d65aad10953d9
harshil-virani <harshilvirani072@gmail.com>
2023-08-08 12:03:27 +0530

    initial commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -11,35 +11,23 @@ const Slider = () => {
                     <div className={classes.part1}>
                         <div className={classes.text}>
                             <span className={classes.textWrapper}>Learn skills and technologies </span>
-                                        which are high in demand, with our practical online and offline courses conducted
-                                        <span className={classes.textWrapper}> by industry experts</span>
+                            which are high in demand, with our practical online and offline courses conducted
+                            <span className={classes.textWrapper}> by industry experts</span>
                         </div>
                         <button className={classes.bookClass}>Book a Free Consultation Now</button>
                     </div>
-
-
-                    {/*<div className={classes.textBox}>*/}
-                    {/*    <div>*/}
-                    {/*        <div className={classes.text}>*/}
-                    {/*            <span className={classes.textWrapper}>Learn skills and technologies </span>*/}
-                    {/*            which are high in demand, with our practical online and offline courses conducted*/}
-                    {/*            <span className={classes.textWrapper}> by industry experts</span>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*    /!*<div>*!/*/}
-                    {/*        <button className={classes.bookClass}>Book a Free Consultation Now</button>*/}
-                    {/*    /!*</div>*!/*/}
-                    {/*</div>*/}
-                    <div className={classes.img}>
-                        <div className={classes.activeStudents}>
-                            <img className={classes.icon} src={icon1} alt="icon1"/>
-                            <span className={classes.text2}>1000+</span>
-                            <span className={classes.text3}>Active Students</span>
-                        </div>
-                        <div className={classes.interactiveCourse}>
-                            <img className={classes.icon} src={icon2} alt="icon2"/>
-                            <span className={classes.text2}>60+</span>
-                            <span className={classes.text3}>Interactive Courses</span>
+                    <div className={classes.part2}>
+                        <div className={classes.img}>
+                            <div className={classes.activeStudents}>
+                                <img className={classes.icon} src={icon1} alt="icon1"/>
+                                <span className={classes.text2}>1000+</span>
+                                <span className={classes.text3}>Active Students</span>
+                            </div>
+                            <div className={classes.interactiveCourse}>
+                                <img className={classes.icon} src={icon2} alt="icon2"/>
+                                <span className={classes.text2}>60+</span>
+                                <span className={classes.text3}>Interactive Courses</span>
+                            </div>
                         </div>
                     </div>
                 </div>
